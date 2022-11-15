ddc9ae92d0b6f2e427b83428ce99c24f735328c3
choi1087 <adcdm87@gmail.com>
2022-11-15 15:10:57 +0900

    initial commit

---
+++ homepjt/src/main.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from './plugins/vuetify'
+
+Vue.config.productionTip = false;
+
+Vue.use(Vuetify);
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: (h) => h(App)
+}).$mount("#app");
