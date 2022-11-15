995a74685409854168df4bb6d9be13890a1718f4
choi1087 <adcdm87@gmail.com>
2022-11-15 17:15:16 +0900

    boardList 불러오기, table 생성

--- homepjt/src/components/board/BoardList.vue
+++ homepjt/src/components/board/BoardList.vue
@@ -1,9 +1,50 @@
 <template>
-  <div>boardList</div>
+  <div>
+    <v-data-table :headers="headers" :items="boardList" :items-per-page="5" class="elevation-1"> </v-data-table>
+    <div class="text-center">
+      <v-pagination v-model="page" :length="6"></v-pagination>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapState, mapActions } from "vuex";
+export default {
+  data() {
+    return {
+      page: 1,
+      headers: [
+        {
+          text: "articleno",
+          value: "articleno",
+          // sortable: true,
+        },
+        {
+          text: "userid",
+          value: "userid",
+        },
+        {
+          text: "subject",
+          value: "subject",
+        },
+        {
+          text: "content",
+          value: "content",
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapState(["boardList"]),
+  },
+  created: {},
+  mounted() {
+    this.searchBoardList();
+  },
+  methods: {
+    ...mapActions(["searchBoardList"]),
+  },
+};
 </script>
 
 <style></style>
--- homepjt/src/store/index.js
+++ homepjt/src/store/index.js
@@ -1,17 +1,33 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import http from "@/api/http";
+// import createPersistedState from "vuex-persistedstate";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    boardList: [],
+    board: null,
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    SEARCH_BOARD_LIST(state, payload) {
+      state.boardList = payload;
+    },
   },
   actions: {
+    searchBoardList({ commit }) {
+      http.get(`/board`).then(({ data }) => {
+        commit("SEARCH_BOARD_LIST", data);
+      });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+  // plugins: [
+  //   createPersistedState({
+  //     // 브라우저 종료시 제거하기 위해 localStorage가 아닌 sessionStorage로 변경. (default: localStorage)
+  //     storage: sessionStorage,
+  //   }),
+  // ],
+});
