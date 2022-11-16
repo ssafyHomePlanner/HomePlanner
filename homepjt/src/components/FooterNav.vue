e24be2eddc49b71c7eb89cb6dc44d18042013015
choi1087 <adcdm87@gmail.com>
2022-11-16 15:14:36 +0900

    [UPDATE] HeaderNav, FooterNav, logo 추가

--- homepjt/src/components/FooterNav.vue
+++ homepjt/src/components/FooterNav.vue
@@ -1,9 +1,27 @@
 <template>
-  <div></div>
+  <v-footer dark padless>
+    <v-card class="flex" flat tile>
+      <v-card-text class="py-2 white--text text-center">
+        <strong>@2020 made by 최규림, 최정온</strong>
+
+        <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+    </v-card>
+  </v-footer>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      icons: ["mdi-github"],
+    };
+  },
+};
 </script>
 
 <style></style>
