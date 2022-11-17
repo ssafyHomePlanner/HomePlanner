cea4f7abb09f24901b5ef090f6db5b310c008e44
jo <chjo0330@gmail.com>
2022-11-17 13:22:33 +0900

    [UPDATE] API 파일 분리

---
+++ homepjt/src/api/board.js
@@ -0,0 +1,39 @@
+import { apiInstance } from "./index.js";
+
+const api = apiInstance();
+
+function listArticle(page, success, fail) {
+  api.get(`/board/${page}`).then(success).catch(fail);
+}
+
+function writeArticle(article, success, fail) {
+  api.post(`/board/write`, JSON.stringify(article)).then(success).catch(fail);
+}
+
+function getArticle(param, success, fail) {
+  api
+    .get(`/board/detail/${param.boardId}/${pram.flag}`)
+    .then(success)
+    .catch(fail);
+}
+
+function modifyArticle(article, success, fail) {
+  api.put(`/board/update`, JSON.stringify(article)).then(success).catch(fail);
+}
+
+function deleteArticle(boardId, success, fail) {
+  api.delete(`/board/delete/${boardId}`).then(success).catch(fail);
+}
+
+function getArticleCommentList(boardId, success, fail) {
+  api.get(`/board/comment/${boardId}`).then(success).catch(fail);
+}
+
+export {
+  listArticle,
+  writeArticle,
+  getArticle,
+  modifyArticle,
+  deleteArticle,
+  getArticleCommentList,
+};
