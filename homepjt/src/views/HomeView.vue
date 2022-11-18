b05107f2ce05854cb436a9958b075fdcc664876b
ėĩė ė¨ <chjo0330@gmail.com>
2022-11-18 10:53:47 +0900

    [UPDATE]  메인 페이지 다지안 작업 중

--- homepjt/src/views/HomeView.vue
+++ homepjt/src/views/HomeView.vue
@@ -2,20 +2,34 @@
   <div class="item">
     <div class="item-bg">
       <div class="item-header-text">Home Planner</div>
-      <div class="item-header-subtext">내 집 마련, 언제까지 생각만 할껀가요?</div>
+      <div class="item-header-subtext">
+        내 집 마련, 언제까지 생각만 할껀가요?
+      </div>
       <v-container>
         <v-row align="center" justify="center">
-          <v-sheet rounded="xl" class="item-header-card" color="white" elevation="8" height="185" width="825">
+          <v-sheet
+            rounded="xl"
+            class="item-header-card"
+            color="white"
+            elevation="8"
+            height="185"
+            width="825"
+          >
             <v-container fill-height fluid>
               <v-text-field
-              class="mt-5 ml-2 mr-2"
+                class="mt-5 ml-8 mr-8"
                 v-model="aptName"
                 solo
                 label="아파트를 검색해보세요."
               ></v-text-field>
               <v-row align="center" justify="center">
                 <v-col cols="3">
-                  <v-select v-model="sidoName" :items="sidoList" label="시도 선택" @change="makeGugunList"></v-select>
+                  <v-select
+                    v-model="sidoName"
+                    :items="sidoList"
+                    label="시도 선택"
+                    @change="makeGugunList"
+                  ></v-select>
                 </v-col>
                 <v-col cols="3">
                   <v-select
@@ -26,7 +40,11 @@
                   ></v-select>
                 </v-col>
                 <v-col cols="3">
-                  <v-select v-model="dongName" :items="dongList" label="읍면동 선택"></v-select>
+                  <v-select
+                    v-model="dongName"
+                    :items="dongList"
+                    label="읍면동 선택"
+                  ></v-select>
                 </v-col>
               </v-row>
             </v-container>
@@ -34,6 +52,65 @@
         </v-row>
       </v-container>
     </div>
+    <v-container class="mt-5 mb-10 text-center" >
+      <div class="item-middle-text">아파트 구매 계획 세우기</div>
+      <div class="item-middle-subtext mt-2">원하는 아파트를 찾으셨나요?</div>
+      <v-container class="mt-8 mb-8">
+        <v-row justify="space-between">
+            <v-sheet height="278" width="214" elevation="4" class="pt-7">
+              <v-row justify="center" class="mt-5">
+                <v-icon size="109" color="green darken-2"> mdi-bank </v-icon>
+              </v-row>
+              <v-row class="item-middle-box-text mt-4" justify="center">
+                저축 계획 세우기
+              </v-row>
+              <v-row justify="center" class="item-middle-box-subtext mt-4">
+                내가 찜한 아파트를 목표로 <br />
+                저축 계획 세우기
+              </v-row>
+            </v-sheet>
+            <v-sheet height="278" width="214" elevation="4" class="pt-7">
+              <v-row justify="center" class="mt-5">
+                <v-icon size="109" color="red darken-2"> mdi-check </v-icon>
+              </v-row>
+              <v-row class="item-middle-box-text mt-4" justify="center">
+                계획 점검하기
+              </v-row>
+              <v-row justify="center" class="item-middle-box-subtext mt-4">
+                내가 목표로 한 저축 계획이 <br />
+                실천할 수 있는 계획인지 체크
+              </v-row>
+            </v-sheet>
+            <v-sheet height="278" width="214" elevation="4" class="pt-7">
+              <v-row justify="center" class="mt-5">
+                <v-icon size="109" color="yellow darken-2"> mdi-clock-outline </v-icon>
+              </v-row>
+              <v-row class="item-middle-box-text mt-4" justify="center">
+                계획 실천하기
+              </v-row>
+              <v-row justify="center" class="item-middle-box-subtext mt-4">
+                내 계획을 잘 지키고 있는지 <br />
+                지속적으로 체크하기
+              </v-row>
+            </v-sheet>
+            <v-sheet height="278" width="214" elevation="4" class="pt-7">
+              <v-row justify="center" class="mt-5">
+                <v-icon size="109" color="blue darken-2"> mdi-home </v-icon>
+              </v-row>
+              <v-row class="item-middle-box-text mt-4" justify="center">
+                내 집 마련 성공!!
+              </v-row>
+              <v-row justify="center" class="item-middle-box-subtext mt-4">
+                드디어 내가 원하던 집을 <br />
+                구매했다!
+              </v-row>
+            </v-sheet>
+        </v-row>
+      </v-container>
+      <v-container fluid style="max-width: 600px;">
+        <v-btn outlined block class="mt-8">구매 계획 세우러 가기</v-btn>
+      </v-container>
+    </v-container>
   </div>
 </template>
 
@@ -112,5 +189,28 @@ export default {
 .item-header-card {
   position: relative;
   top: 140px;
+}
+
+.item-middle-text {
+  font-weight: bold;
+  text-align: center;
+  font-size: 27px;
+}
+.item-middle-subtext {
+  font-weight: bold;
+  text-align: center;
+  font-size: 16px;
+  color: #9f9999;
+}
+
+.item-middle-box-text {
+  font-weight: bold;
+  text-align: center;
+  font-size: 16px;
+}
+.item-middle-box-subtext {
+  text-align: center;
+  font-size: 14px;
+  color: #958E8E;
 }
 </style>
