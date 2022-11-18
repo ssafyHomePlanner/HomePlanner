a6a84e9ae9ebcb07c7535c427855103c5f626e43
ėĩė ė¨ <chjo0330@gmail.com>
2022-11-18 11:39:44 +0900

    .

--- homepjt/src/components/FooterNav.vue
+++ homepjt/src/components/FooterNav.vue
@@ -2,7 +2,7 @@
   <v-footer dark>
     <v-card class="flex" tile>
       <v-card-text class="py-2 white--text text-center">
-        <strong>@2020 made by 최규림, 최정온</strong>
+        <strong>@2022 made by 최규림, 최정온</strong>
 
         <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
           <v-icon size="24px">
