29684e2b8f19e025a57f4a2b11b9f3b8d975754c
jo <chjo0330@gmail.com>
2022-11-21 15:17:03 +0900

    [UPDATE] 아파트 리스트 작업 중

--- homepjt/src/views/apt/AptListView.vue
+++ homepjt/src/views/apt/AptListView.vue
@@ -100,15 +100,14 @@
         </v-row>
       </v-sheet>
     </v-row>
-    <v-row>
-      <v-btn @click="moveResultPage" color="primary">아파트 검색 결과</v-btn>
-    </v-row>
-    <v-row justify="center" class="mt-12">
+
+    <v-row justify="center" class="mt-12 mb-12">
       <v-col class="ml-8 mr-8 pl-8 pr-8">
         <v-data-table
           :headers="headers"
-          :items="desserts"
-          :items-per-page="5"
+          :items="houseInfoList"
+          :items-per-page="10"
+          @click:row="moveResultPage"
           class="elevation-1"
         ></v-data-table>
       </v-col>
@@ -117,108 +116,35 @@
 </template>
 
 <script>
+import { mapState, mapActions } from "vuex";
+
+const aptStore = "aptStore";
+const houseInfoStore = "houseInfoStore";
+
 export default {
   methods: {
     moveResultPage() {
       this.$router.push({ name: "aptResultView" });
     },
+    ...mapActions(aptStore, [
+      "searchSidoList",
+      "searchGugunList",
+      "searchDongList",
+    ]),
+    ...mapActions(houseInfoStore, ["getHouseInfoList", "getHouseInfoListAuto"]),
+  },
+  computed: {
+    ...mapState(aptStore, ["sidoList", "gugunList", "dongList"]),
+    ...mapState(houseInfoStore, ["houseInfo", "houseInfoList"]),
   },
   data() {
     return {
       headers: [
-        {
-          text: "Dessert (100g serving)",
-          align: "start",
-          sortable: false,
-          value: "name",
-        },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" },
-      ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%",
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%",
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%",
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%",
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%",
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%",
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%",
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%",
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%",
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%",
-        },
+        { text: "건축년도", value: "buildYear" },
+        { text: "아파트 이름", value: "apartmentName" },
+        { text: "동이름", value: "dong" },
+        { text: "조회수", value: "viewCnt" },
+        { text: "좋아요", value: "likeCnt" },
       ],
     };
   },
