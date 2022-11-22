10c1be699da7f81309d2fe0a092092e8dcb3fa06
ėĩė ė¨ <chjo0330@gmail.com>
2022-11-22 21:40:32 +0900

    [UPDATE] 게시판 글쓰기 디자인 업데이트

--- homepjt/src/components/board/item/BoardInputItem.vue
+++ homepjt/src/components/board/item/BoardInputItem.vue
@@ -1,41 +1,48 @@
 <template>
-  <v-sheet
-    rounded="xl"
-    color="white"
-    elevation="5"
-    height="400"
-    width="800"
-    class="ma-8"
-  >
-    <v-container class="pa-6">
-      <v-row class="mb-1">
-        <v-col style="text-align: left">
-          <v-form ref="form" lazy-validation>
-            <v-text-field
-              v-model="article.title"
-              :rules="titleRules"
-              label="제목을 입력하세요."
-              required
-            ></v-text-field>
-            <v-textarea
-              @keyup.enter="registArticle"
-              v-model="article.content"
-              :rules="contentRules"
-              label="내용을 입력하세요."
-              required
-            ></v-textarea>
-          </v-form>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col style="text-align: right">
-          <v-btn @click="registArticle" color="primary" elevation="2"
-            >글작성</v-btn
-          >
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-sheet>
+  <v-container fill-height fluid c>
+    <v-row justify="center" class="mt-8 mb-4">
+      <h2>게시판 글쓰기</h2>
+    </v-row>
+    <v-row justify="center">
+      <v-sheet
+        rounded="xl"
+        color="white"
+        elevation="5"
+        height="400"
+        width="800"
+        class="ma-8"
+      >
+        <v-container class="pa-6">
+          <v-row class="mb-1">
+            <v-col style="text-align: left">
+              <v-form ref="form" lazy-validation>
+                <v-text-field
+                  v-model="article.title"
+                  :rules="titleRules"
+                  label="제목을 입력하세요."
+                  required
+                ></v-text-field>
+                <v-textarea
+                  @keyup.enter="registArticle"
+                  v-model="article.content"
+                  :rules="contentRules"
+                  label="내용을 입력하세요."
+                  required
+                ></v-textarea>
+              </v-form>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col style="text-align: right">
+              <v-btn @click="registArticle" color="primary" elevation="2"
+                >글작성</v-btn
+              >
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-sheet>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
